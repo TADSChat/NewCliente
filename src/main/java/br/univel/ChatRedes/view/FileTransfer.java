47247beb706ce10151672f7f38aa38fc4d4556aa
Eliel <eliel.dourado>
2017-04-23 22:24:50 -0300

    Corrigindo File Transfer.

--- src/main/java/br/univel/ChatRedes/view/FileTransfer.java
+++ src/main/java/br/univel/ChatRedes/view/FileTransfer.java
@@ -39,14 +39,14 @@ public class FileTransfer extends JFrame {
 	public FileTransfer(EntidadeUsuario remetente, File arquivo, byte[] bytes) {
 
 		setVisible(true);
-		setTitle("File Transfer");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500, 200);
+		setTitle("Arquivo");
+		setSize(420, 260);
 		setLocation((dimensaoTela.width - this.getSize().width) / 2, (dimensaoTela.height - this.getSize().height) / 2);
-
+		
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+		contentPane.setSize(400, 200);
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[] { 130, 130, 221, 0 };
@@ -59,7 +59,7 @@ public class FileTransfer extends JFrame {
 		lblDesejaReceberEste.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 15));
 		GridBagConstraints gbc_lblDesejaReceberEste = new GridBagConstraints();
 		gbc_lblDesejaReceberEste.anchor = GridBagConstraints.EAST;
-		gbc_lblDesejaReceberEste.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDesejaReceberEste.insets = new Insets(5, 0, 0, 5);
 		gbc_lblDesejaReceberEste.gridx = 0;
 		gbc_lblDesejaReceberEste.gridy = 0;
 		contentPane.add(lblDesejaReceberEste, gbc_lblDesejaReceberEste);
@@ -70,7 +70,7 @@ public class FileTransfer extends JFrame {
 		gbc_lblRemetente.fill = GridBagConstraints.HORIZONTAL;
 		gbc_lblRemetente.anchor = GridBagConstraints.NORTH;
 		gbc_lblRemetente.gridwidth = 2;
-		gbc_lblRemetente.insets = new Insets(0, 0, 5, 0);
+		gbc_lblRemetente.insets = new Insets(5, 0, 0, 5);
 		gbc_lblRemetente.gridx = 1;
 		gbc_lblRemetente.gridy = 0;
 		contentPane.add(lblRemetente, gbc_lblRemetente);
@@ -78,7 +78,7 @@ public class FileTransfer extends JFrame {
 		JLabel lblNomeDoArquivo = new JLabel("Nome do Arquivo:");
 		lblNomeDoArquivo.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 15));
 		GridBagConstraints gbc_lblNomeDoArquivo = new GridBagConstraints();
-		gbc_lblNomeDoArquivo.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNomeDoArquivo.insets = new Insets(5, 0, 0, 5);
 		gbc_lblNomeDoArquivo.anchor = GridBagConstraints.EAST;
 		gbc_lblNomeDoArquivo.gridx = 0;
 		gbc_lblNomeDoArquivo.gridy = 1;
@@ -90,13 +90,13 @@ public class FileTransfer extends JFrame {
 		gbc_lblArqNome.fill = GridBagConstraints.HORIZONTAL;
 		gbc_lblArqNome.anchor = GridBagConstraints.NORTH;
 		gbc_lblArqNome.gridwidth = 2;
-		gbc_lblArqNome.insets = new Insets(0, 0, 5, 0);
+		gbc_lblArqNome.insets = new Insets(5, 0, 0, 5);
 		gbc_lblArqNome.gridx = 1;
 		gbc_lblArqNome.gridy = 1;
 		contentPane.add(lblArqNome, gbc_lblArqNome);
 
 		JButton btnAceitar = new JButton("Aceitar");
-		btnAceitar.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 14));
+		btnAceitar.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 16));
 		btnAceitar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String path = "";
@@ -117,8 +117,8 @@ public class FileTransfer extends JFrame {
 				try {
 					Files.write(Paths.get(novoArquivo.getPath()), bytes, StandardOpenOption.CREATE);
 				} catch (IOException exception) {
-					JOptionPane.showMessageDialog(null,
-							String.format("Erro ao criar o arquivo [%s]! \n%s", arquivo.getName(), exception.getCause().toString()));
+					JOptionPane.showMessageDialog(null, String.format("Erro ao criar o arquivo [%s]! \n%s",
+							arquivo.getName(), exception.getCause().toString()));
 					return;
 				}
 
@@ -132,20 +132,20 @@ public class FileTransfer extends JFrame {
 		gbc_btnAceitar.gridwidth = 3;
 		gbc_btnAceitar.anchor = GridBagConstraints.NORTH;
 		gbc_btnAceitar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnAceitar.insets = new Insets(5, 0, 5, 0);
+		gbc_btnAceitar.insets = new Insets(10, 30, 10, 30);
 		gbc_btnAceitar.gridx = 0;
 		gbc_btnAceitar.gridy = 2;
 		contentPane.add(btnAceitar, gbc_btnAceitar);
 
 		JButton btnRecusar = new JButton("Recusar");
-		btnRecusar.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 14));
+		btnRecusar.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 16));
 		btnRecusar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				dispose();
 			}
 		});
 		GridBagConstraints gbc_btnRecusar = new GridBagConstraints();
-		gbc_btnRecusar.insets = new Insets(5, 0, 5, 0);
+		gbc_btnRecusar.insets = new Insets(3, 30, 10, 30);
 		gbc_btnRecusar.gridwidth = 3;
 		gbc_btnRecusar.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnRecusar.anchor = GridBagConstraints.NORTH;
